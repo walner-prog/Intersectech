ceeb31a2e8e9813cbe832dc64cfbca51995263ec
walner <git@walner.cl>
2024-04-28 21:29:30 -0600

    tercer comit de blog

--- src/views/AcercaDe.vue
+++ src/views/AcercaDe.vue
@@ -4,23 +4,41 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-8 mx-auto text-center">
-              <h2 class="section-heading">Acerca de Intersectech</h2>
+              <h2 class="section-heading text-orange">Acerca de Intersectech</h2>
               <hr class="my-4">
-              <p class="mb-4">
-                ¡Bienvenido a <strong>Intersectech</strong>! Soy un desarrollador web apasionados por la tecnología digital y estoy emocionados de compartir este  proyecto contigo.
-              </p>
-              <p class="mb-4">
-                En <strong>Intersectech</strong>, me  esfuerzo por ofrecer contenido educativo y actualizado sobre las últimas tendencias acerca de la  tecnología en general y temas tecnológicos que seguro seran de tu interes.
-              </p>
-              <p class="mb-4">
-                Siempre estoy explorando nuevas herramientas, técnicas y tendencias en el mundo digital como programador web, y me encanta compartir mis experiencias y descubrimientos con la  comunidad de lectores.
-              </p>
-              <p class="mb-4">
-                ¿Tienes alguna pregunta, comentario o consultar algo referente al desarrollo web ? ¡me  encantaría saber de ti! No dudes en ponerte en contacto  a través de mi correo electrónico <a href="mailto:ca140611@gmail.com">ca140611@gmail.com</a> o en nuestras redes sociales.
-              </p>
+              <div class="card mb-4" style="background-color: rgb(0, 12, 116);">
+                <div class="card-body">
+                  <p class="card-text text-white mb-4">
+                    ¡Bienvenido a <strong>Intersectech</strong>! Soy un desarrollador web apasionado por la tecnología digital y estoy emocionado de compartir este proyecto contigo.
+                  </p>
+                </div>
+              </div>
+              <div class="card mb-4" style="background-color: rgb(0, 12, 116);">
+                <div class="card-body">
+                  <p class="card-text text-white mb-4">
+                    En <strong>Intersectech</strong>, me esfuerzo por ofrecer contenido educativo y actualizado sobre las últimas tendencias en tecnología y temas tecnológicos que seguro serán de tu interés.
+                  </p>
+                </div>
+              </div>
+              <div class="card mb-4" style="background-color: rgb(0, 12, 116);">
+                <div class="card-body">
+                  <p class="card-text text-white mb-4">
+                    Siempre estoy explorando nuevas herramientas, técnicas y tendencias en el mundo digital como programador web, y me encanta compartir mis experiencias y descubrimientos con la comunidad de lectores.
+                  </p>
+                </div>
+              </div>
+              <div class="card mb-4" style="background-color: rgb(0, 12, 116);">
+                <div class="card-body">
+                  <p class="card-text text-white mb-0">
+                    ¿Tienes alguna pregunta, comentario o deseas consultar algo relacionado con el desarrollo web? ¡Me encantaría saber de ti! No dudes en ponerte en contacto a través de mi correo electrónico <a href="mailto:ca140611@gmail.com">ca140611@gmail.com</a> o en nuestras redes sociales.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+        
+        
       </section>
       
 
@@ -84,5 +102,8 @@
   
   <style>
   /* Estilos CSS */
+  .text-orange{
+    color: orange;
+  }
   </style>
   
